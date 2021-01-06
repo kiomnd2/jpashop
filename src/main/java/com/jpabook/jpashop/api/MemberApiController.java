8e0f339f6ee24b0499fcb9632723078ed2fc4710
kiomnd2 <kiomnd2@gmail.com>
2021-01-07 01:29:59 +0900

    회원 등록 및 조회 api

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -1,9 +1,108 @@
 package com.jpabook.jpashop.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController // Controller + ResponseBody
+@RequiredArgsConstructor
 public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/api/v1/members") // member 엔티티 직접적으로 노출함..
+    public List<Member> membersV1() {
+        return memberService.findMember();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMember();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
+
+    @PostMapping("/api/v1/members") // Entity 자체를 직접 파라미터로 받는것은 매우 위험, 스펙이 변경되었을 때 entity 가 직접적으로 영향
+    public CreateMemberResponse saveMEmberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members") // api 스펙을 바꾸면 변환 시점에 오류남
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        // update 다음에 가급적 Entity를다시 반환하지 마라
+        memberService.update(id, request.getName());
+
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+
+    @Data
+    public class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class UpdateMemberResponse {
+
+        private Long id;
+        private String name;
+    }
+
+
+    @Data
+    public class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 
 
 }
